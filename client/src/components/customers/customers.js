12060029e2968d7d3eb0236a9a9338b266f47e31
jimmychen10 <jimmychen1996@yahoo.com>
2019-12-18 13:40:21 -0800

    testing

---
+++ client/src/components/customers/customers.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import './customers.css';
+
+class Customers extends Component{
+
+constructor(){
+    super();
+    this.state = {
+        customers: []
+    }
+}
+
+componentDidMount(){
+    fetch('/api/customers')
+    .then(res => res.json())
+    .then(customers => this.setState({customers}, () => console.log('Customers fetched..',customers)));
+}
+
+reder() {
+
+  return (
+    <div >
+        <h2>Customers</h2>
+    </div>
+  );
+}
+}
+
+export default Customers;
